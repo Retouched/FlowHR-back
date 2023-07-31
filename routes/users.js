87283c006748a90f674b2ba275d3aaf9bfd3c626
Romain <pro.vuillaume@gmail.com>
2023-07-31 11:52:25 +0200

    Création du module checkBody, insertion de la condition de creation d'un nouveau collaborateur seulement si l'adresse mail n'existe pas déjà

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,26 @@ var router = express.Router();
 
 require("../models/connection");
 const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
 
 // ROUTE ADD USER
 router.post("/", (req, res) => {
+  //AJOUT DU MODULE CHECKBODY
+  if (
+    !checkBody(req.body, [
+      "firstname",
+      "lastname",
+      "password",
+      "email",
+      "department",
+      "job",
+      "role",
+    ])
+  ) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
   //DEFINITION DE L'OBJET RECU PAR LE FRONT
   const { firstname, lastname, password, email, department, job, role } =
     req.body;
@@ -21,8 +38,15 @@ router.post("/", (req, res) => {
 
   console.log("newUser", newUser);
 
-  newUser.save().then(() => {
-    res.json({ result: true });
+  //CONDITION DE CREATION SUR L'ADRESSE MAIL : si elle existe déjà result : false
+  User.findOne({ email: req.body.email }).then((data) => {
+    if (data === null) {
+      newUser.save().then(() => {
+        res.json({ result: true });
+      });
+    } else {
+      res.json({ result: false });
+    }
   });
 });
 
