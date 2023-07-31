29431fc93484ccb099c4619f5963f23481e4d79d
Romain <pro.vuillaume@gmail.com>
2023-07-31 09:59:40 +0200

    Insertion connexion string; création du dossier models ainsi que du model users.js

---
+++ models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  lastname: String,
+  firstname: String,
+  email: String,
+  token: String,
+  department: [{ type: mongoose.Schema.Types.ObjectId, ref: "departments" }],
+  connected: Boolean,
+  job: [{ type: mongoose.Schema.Types.ObjectId, ref: "jobs" }],
+  role: String,
+  password: String,
+  internalCompany: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "internalCompanies" },
+  ],
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
