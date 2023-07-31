32629a7572c10ce39535b4129a9e44286699c89d
Romain <pro.vuillaume@gmail.com>
2023-07-31 12:45:38 +0200

    Creation du model hireRequests

---
+++ models/hireRequest.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const hireRequestSchema = mongoose.Schema({
+  numRequest: Number,
+  goalRequest: { type: mongoose.Schema.Types.ObjectId, ref: "goalRequests" },
+  nameReplacedPerson: String,
+  lastnameReplacedPerson: String,
+  job: { type: mongoose.Schema.Types.ObjectId, ref: "jobs" },
+  newJob: String,
+  classification: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "classifications",
+  },
+  firstnameManager: String,
+  lastnameManager: String,
+  department: { type: mongoose.Schema.Types.ObjectId, ref: "departments" },
+  contractType: { type: mongoose.Schema.Types.ObjectId, ref: "contractTypes" },
+  dateHireRequest: Date,
+  dpRequestStatus: Boolean,
+  drhRequestStatus: Boolean,
+  dafRequestStatus: Boolean,
+  pdgRequestStatus: Boolean,
+  globalRequestStatus: Boolean,
+  dpComment: String,
+  drhComment: String,
+  dafComment: String,
+  pdgComment: String,
+  pourcentageWorkTime: Number,
+  contractReason: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "contractReasons",
+  },
+  startDateContract: Date,
+  endDateContract: Date,
+  durationContractDay: Number,
+  durationContractMonth: Number,
+  minimumWage: Number,
+  maximumWage: Number,
+  monthlyVariableWage: Boolean,
+  monthlyVariableWageAmount: Number,
+  annualVariableWage: Boolean,
+  annualVariableWageAmount: Number,
+  moveAssist: Boolean,
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+});
+
+const HireRequest = mongoose.model("hireRequests", hireRequestSchema);
+
+module.exports = HireRequest;
